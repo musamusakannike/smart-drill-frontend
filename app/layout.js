0450a046950520f5f3b60c4e21c93e11a5f068df
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2024-12-15 22:35:02 +0100

    Used Inter font throughout the app

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,11 @@
-import localFont from "next/font/local";
+import {Inter} from "next/font/google"
 import "./globals.css";
 import { ToastProvider } from "@/contexts/ToastContext";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+})
 
 export const metadata = {
   title: "Smart Drill",
@@ -23,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className}`}
       >
         <ToastProvider>{children}</ToastProvider>
       </body>
