a5fbf85484f1ab4fe0b403839f5f275bd276a086
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-01-05 04:18:43 +0100

    Added async await functionality to server page

--- app/(main)/community/[communityId]/page.jsx
+++ app/(main)/community/[communityId]/page.jsx
@@ -1,8 +1,8 @@
 import CommunityPageClient from "@/components/CommunityPage/CommunityPageClient";
 
 // Server Component
-export default function Page({ params }) {
-  const { communityId } = params;
+export default async function Page({ params }) {
+  const { communityId } = await params;
 
   return <CommunityPageClient communityId={communityId} />;
 }
